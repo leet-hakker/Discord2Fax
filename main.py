436158af9962fe9b07d9ad22e02cd5ce738550bf
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-01-22 21:59:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ command_prefix = ")"
 #Assigning bot to a variable which contains the access to commands
 bot = commands.Bot(command_prefix)
 
-#Creating a bot event listener fro messages
+#Creating a bot event listener for messages
 @bot.event
 async def on_message(message):
-	
+	#Creating a variable that contains the message
+	message = message.content
+	#Outputting the message for testing
+	print(message)
+
+#This is the client secret. Do not share it with anyone, otherwise they will have full access to the application.
+bot.run("S8rrEcnqt6OW1-NVBzaq0S2x1Rm_Z9VJ")
