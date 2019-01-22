01ccd014225a6d7cbe4ba324414ef63ea211b051
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-01-22 21:37:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-#Importing the necesary libraries
-import Discord
+#Importing the necessary libraries
+import discord
 import asyncio
 
+#This will not be used, but is necessary
+command_prefix = ")"
+
+#Assigning bot to a variable which contains the access to commands
 bot = commands.Bot(command_prefix)
+
+#Creating a bot event listener fro messages
+@bot.event
+async def on_message(message):
+	
