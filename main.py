bce5d0ff4c351408184983592275f26f1d4a775b
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-01-23 18:18:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Importing the necessary libraries
 import discord
+from discord.ext import commands
+from discord.ext.commands import bot
 import asyncio
 
 #This will not be used, but is necessary
@@ -7,6 +9,11 @@ command_prefix = ")"
 
 #Assigning bot to a variable which contains the access to commands
 bot = commands.Bot(command_prefix)
+
+#Telling me when it is online
+@bot.event
+async def on_ready():
+	print("Ready when you are")
 
 #Creating a bot event listener for messages
 @bot.event
@@ -17,4 +24,4 @@ async def on_message(message):
 	print(message)
 
 #This is the client secret. Do not share it with anyone, otherwise they will have full access to the application.
-bot.run("S8rrEcnqt6OW1-NVBzaq0S2x1Rm_Z9VJ")
+bot.run("NTM3MzU5MzI0NTE5ODU4Mjgx.Dyo7vg.BRpL9xFl_B-mMNa5TMO7kANLlHg")
