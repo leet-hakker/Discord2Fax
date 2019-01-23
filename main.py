cc577985696bae7c23b279689ee9fe178c232fe9
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-01-23 19:19:06 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Setting the chartype
+# -*- coding: <utf-8> -*-
+
 #Importing the necessary libraries
 import discord
 from discord.ext import commands
@@ -13,15 +16,18 @@ bot = commands.Bot(command_prefix)
 #Telling me when it is online
 @bot.event
 async def on_ready():
-	print("Ready when you are")
+	print("Ready when you are\n\n")
 
 #Creating a bot event listener for messages
 @bot.event
 async def on_message(message):
-	#Creating a variable that contains the message
-	message = message.content
-	#Outputting the message for testing
-	print(message)
+        #Opening a text file as a variable for easy access
+        text_file = open("print.txt", "a+")
+        #Creating a variable that contains the message
+        message = message.content
+        #Outputting the message for testing
+        text_file.write("\n" + message)
+        text_file.close
 
 #This is the client token. Do not share it with anyone, otherwise they will have full access to the application.
 bot.run("NTM3MzU5MzI0NTE5ODU4Mjgx.Dyo7vg.BRpL9xFl_B-mMNa5TMO7kANLlHg")
